c5d120d611eb6551a4254b6c5388f96c82332e69
aditya verma <ad4545a@gmail.com>
2025-08-07 14:46:47 +0530

    this is initialization

--- claculater.java
+++ claculater.java
@@ -1,1 +1,6 @@
-
+public class claculater {
+   public static void main(String[] args) {
+    System.out.println("This is a simple calculator program.");
+    // You can add more functionality here later
+   } 
+}
