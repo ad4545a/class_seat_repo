dc19050d0fe8d951570876a965869b9d636eed6f
Abhiyansh29 <abhiyanshvarshney29@gmail.com>
2025-08-07 14:53:51 +0530

    Abhiyansh input create

--- claculater.java
+++ claculater.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class claculater {
    public static void main(String[] args) {
     System.out.println("This is a simple calculator program.");
     // You can add more functionality here later
+    Scanner scn=new Scanner(System.in);
+    System.out.println("Enter first Number");
+    int a=scn.nextInt();
+    System.out.println("Enter second Number");
+    int b=scn.nextInt();
+    System.out.println("Enter the operation");
+    String inp=scn.next();
+    if(inp.equals("add")){
+        add(a,b);
+    }
+    else if(inp.equals("multiply")){
+        multiply(a,b);
+    }
+    else if(inp.equals("subtract")){
+        subtract(a,b);
+    }
+    else if(inp.equals("divide")){
+        divide(a,b);
+    }
+    else{
+        System.out.println("bhai shi operation likh");
+    }
+
+    
    } 
 }
