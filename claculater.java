1d113e4ed6475dd6faede5100a5887b23a268107
aditya verma <ad4545a@gmail.com>
2025-08-07 14:52:14 +0530

    all function is done by aditya verma

--- claculater.java
+++ claculater.java
@@ -2,5 +2,21 @@ public class claculater {
    public static void main(String[] args) {
     System.out.println("This is a simple calculator program.");
     // You can add more functionality here later
+
    } 
+   public static int add(int a, int b) {
+       return a + b;
+   }
+    public static int subtract(int a, int b) {
+         return a - b;
+    }
+    public static int multiply(int a, int b) {
+            return a * b;
+    }
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
 }
